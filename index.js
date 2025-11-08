f8de45b9a0a549d2dd47ea39fd3c3d9474edffb8
ebrahim2355 <web.ebrahimali@gmail.com>
2025-11-09 02:05:13 +0600

    mongodb connected

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rlqi2bh.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("My Own Server is Running")
+})
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db("food-lovers");
+        const reviewCollection = db.collection("reviews");
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Smart Server is Running on Port: ${port}`);
+})
